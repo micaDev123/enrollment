421d104be40e6c1f1cc473b5c6a073fc40aa2b77
Mike <catapangmichaelsuarez@gmail.com>
2024-05-29 17:56:18 +0800

    config

--- application/config/config.php
+++ application/config/config.php
@@ -357,13 +357,15 @@ $config['encryption_key'] = 'OptimumlinkupSoftware';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'database';
-$config['sess_cookie_name'] = 'ci_session';
-$config['sess_expiration'] = 7200;
-$config['sess_save_path'] = 'ci_sessions';
-$config['sess_match_ip'] = FALSE;
-$config['sess_time_to_update'] = 300;
-$config['sess_regenerate_destroy'] = FALSE;
+$config['sess_cookie_name']     = 'ci_session';
+$config['sess_expiration']      = 7200;
+$config['sess_expire_on_close'] = FALSE;
+$config['sess_encrypt_cookie']  = FALSE;
+$config['sess_use_database']    = TRUE; # change this to TRUE
+$config['sess_table_name']      = 'ci_sessions'; #table name
+$config['sess_match_ip']        = FALSE;
+$config['sess_match_useragent'] = TRUE;
+$config['sess_time_to_update']  = 300;
 
 /*
 |--------------------------------------------------------------------------
