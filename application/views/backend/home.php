89f8e3a99724ad8461d5ecabe8d39514005c5781
Mike <catapangmichaelsuarez@gmail.com>
2024-05-31 04:11:12 +0800

    update link in bootstrap icon

--- application/views/backend/home.php
+++ application/views/backend/home.php
@@ -18,7 +18,7 @@
 
   <!-- Vendor CSS Files -->
   <link href="<?php echo base_url(); ?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="<?php echo base_url(); ?>assets/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="<?php echo base_url(); ?>assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
   <link href="<?php echo base_url(); ?>assets/vendor/aos/aos.css" rel="stylesheet">
   <link href="<?php echo base_url(); ?>assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
   <link href="<?php echo base_url(); ?>assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
